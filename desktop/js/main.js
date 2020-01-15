8ba823b8ff27780b0978614d5b0e2c8319b9da78
MAKIO135 <makio135@gmail.com>
2020-01-15 23:19:28 +0100

    rename elements + start server addition in html

--- desktop/js/main.js
+++ desktop/js/main.js
@@ -2,33 +2,34 @@ const { app } = require('electron').remote
 
 let serialPort
 let parser
-const portSelect = document.querySelector('#port')
-const rateSelect = document.querySelector('#baudrate')
-const connectButton = document.querySelector('#connect')
+const serialPortSelect = document.querySelector('#serial-port')
+const serialBaudrateSelect = document.querySelector('#serial-baudrate')
+const serialConnect = document.querySelector('#serial-connect')
+const serialData = document.querySelector('#serial-data')
 
 app.scannPorts().then(ports => {
-    portSelect.innerHTML = `<option value="">--Select port--</option>`
+    serialPortSelect.innerHTML = `<option value="">--Select port--</option>`
 
     ports.forEach(port => {
         const option = document.createElement('option')
         const { path, manufacturer } = port
         option.innerText = path + (manufacturer ? ` ${manufacturer}` : '')
         option.value = path
-        portSelect.appendChild(option)
+        serialPortSelect.appendChild(option)
     })
 })
 
-connectButton.addEventListener('click', e => {
-    if(portSelect.value) {
-        const connection = app.connectPort(portSelect.value, parseInt(rateSelect.value))
+serialConnect.addEventListener('click', e => {
+    if(serialPortSelect.value) {
+        const connection = app.connectPort(serialPortSelect.value, parseInt(serialBaudrateSelect.value))
         serialPort = connection.serialPort
         parser = connection.parser
 
         // serialPort.on('open', () => {
-            // console.log(`${portSelect.value} open`)
+            // console.log(`${serialPortSelect.value} open`)
         
             parser.on('data', data => {
-                console.log('data from Serial: ' + data)
+                serialData.innerText = data
                 app.emitMessage(data)
             })
         // })
